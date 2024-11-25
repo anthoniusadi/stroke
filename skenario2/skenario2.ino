8618a3a65085a1733ed925b3ecfd64e150a08e0f
anthoniusadi <anthonyfix0@gmail.com>
2024-11-25 21:37:54 +0700

    mqtt add, ubah pin analog

--- skenario2/skenario2.ino
+++ skenario2/skenario2.ino
@@ -1,5 +1,24 @@
+// //mqtt
+#include <WiFi.h>
+#include <PubSubClient.h>
+const char *ssid = "IoT_stroke";
+const char *password = "iotstroke";
 
-int sensor_grove = 26;
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "latihan1/capaian";
+const char *topic2 = "nama";
+
+
+
+const char *mqtt_username = "qwerty";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int sensor_grove = 32;
 int sensor_myo = 34;
 int value = 0;
 int signal_wave = 0;
@@ -12,20 +31,113 @@ int offset = 200;
 bool status_1 = false;
 bool status_2 = false;
 bool status_3 = false;
-int progress_bar=0;
+bool ready = false;
+int progress_bar = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
   pinMode(sensor_grove, INPUT);
   pinMode(sensor_myo, INPUT);
 
   for (int i = 0; i < arraySize; i++) {
     dataArray[i] = 0;
   }
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  //connecting to a mqtt broker
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    String client_id = "esp8266-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("Public emqx mqtt broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+    client.publish(topic2, "pasien_1");
+  }
+  //setup wifi
+  // setup_wifi();
+  // client.setServer(mqtt_broker, 1883);
+  // client.setCallback(callback);
+  // client.subscribe("latihan1/capaian");
+}
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
+// void setup_wifi() {
+//   delay(10);
+//   // Serial.println();
+//   // Serial.print("Connecting to ");
+//   // Serial.println(ssid);
+
+//   WiFi.begin(ssid, password);
+
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     // Serial.print(".");
+//   }
+
+//   // Serial.println("");
+//   // Serial.println("WiFi connected");
+//   // Serial.println("IP address: ");
+//   // Serial.println(WiFi.localIP());
+// }
+
+// void callback(char *topic, byte *payload, unsigned int length) {
+//   // Serial.print("Message arrived in topic: ");
+//   Serial.println(topic);
+//   // Serial.print("Message:");
+//   for (int i = 0; i < length; i++) {
+//     Serial.print((char)payload[i]);
+//   }
+//   Serial.println();
+//   // Serial.println("-----------------------");
+// }
+// void reconnect() {
+//   // Loop until we're reconnected
+//   while (!client.connected()) {
+//     // Serial.print("Attempting MQTT connection...");
+//     // Attempt to connect
+//     if (client.connect("esp32")) {
+//       // Serial.println("connected");
+//       // Subscribe
+//       client.subscribe("latihan1/capaian");
+//     } else {
+//       // Serial.print("failed, rc=");
+//       // Serial.print(client.state());
+//       // Serial.println(" try again in 5 seconds");
+//       // Wait 5 seconds before retrying
+//       delay(5000);
+//     }
+// }
+// }
 //char data_from_display;
+char msg_out[20];
 void loop() {
+  // if (!client.connected()) {
+  //   reconnect();
+  // }
+
+  delay(1);
   int grove = analogRead(sensor_grove);
   int myo = analogRead(sensor_myo);
   int myosensorVal = map(myo, 0, 4096, 0, 2048);
@@ -33,7 +145,11 @@ void loop() {
   dataArray[currentIndex] = grovesensorVal;
   int grove_kalibrasi = grovesensorVal - kalibrator;
 
+  Serial.print(grove_kalibrasi);
+
+
   while (Serial.available()) {
+
 
     char data_from_display = Serial.read();
     if (data_from_display == 'T') {
@@ -44,6 +160,7 @@ void loop() {
       initial_value = kalibrator;
 
       delay(3000);
+      ready = true;
     }
   }
 
@@ -93,9 +210,12 @@ void loop() {
   Serial.write(0xff);
   Serial.write(0xff);
 
-  if (progress_bar > 55) {
-    status_1 = true;
+  if (progress_bar > 55 && ready==true) {
 
+    if (status_1 == false) {
+      client.publish(topic, "1");
+      // status_1 = true;
+    }
     Serial.print("p0.pic=0");
     Serial.write(0xff);
     Serial.write(0xff);
@@ -112,8 +232,13 @@ void loop() {
     Serial.write(0xff);
     Serial.write(0xff);
   }
-  if (progress_bar > 60) {
-    status_2 = true;
+  if (progress_bar > 60 && ready==true) {
+    if (status_2 == false) {
+      client.publish(topic, "2");
+      // status_2 = true;
+    }
+
+
     Serial.print("p1.pic=0");
     sendTo();
 
@@ -122,10 +247,14 @@ void loop() {
 
     Serial.print("t6.txt=tercapai");
     sendTo();
-
   }
-  if (progress_bar > 70) {
-    status_3 = true;
+  if (progress_bar > 65 && ready==true) {
+    if (status_3 == false) {
+      client.publish(topic, "3");
+      // status_3 = true;
+    }
+
+
     Serial.print("p2.pic=0");
     sendTo();
 
@@ -137,6 +266,7 @@ void loop() {
     sendTo();
     //send light
   }
+  client.loop();
 }
 
 int tare(int data[], int size) {
@@ -155,3 +285,5 @@ void sendTo() {
   Serial.write(0xff);
   Serial.write(0xff);
 }
+// String command = "n0.val=" + String(grove_kalibrasi) + "\xff\xff\xff";
+// Serial.print(command);
